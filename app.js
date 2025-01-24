abf0d95ae1d489f0a674a4de9d922f4d0a9bef05
sebastian <malagonmoraless@gmail.com>
2025-01-24 16:35:24 -0500

    creando arreglo de amigos

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//creacion de la arreglo de nombres de los amigos
+
+let listadeamigos = [];
